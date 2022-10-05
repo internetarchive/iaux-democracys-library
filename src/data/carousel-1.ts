bb492fb8864bdb73135c07112efd5e1736fae196
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-10-05 01:29:18 -0700

    main frame

---
+++ src/data/carousel-1.ts
@@ -0,0 +1,48 @@
+export type CarouselCard = {
+  id: string;
+  title: string;
+  image: string;
+};
+
+export const carousel1: CarouselCard[] = [
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+];
---
+++ src/data/carousel-2.ts
@@ -0,0 +1,44 @@
+import type { CarouselCard } from './carousel-1';
+
+export const carousel2: CarouselCard[] = [
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'NASA_NTRS_Archive_20070019742',
+    title: 'NASA Technical Reports Server (NTRS) 20070019742: Solar Schematic',
+    image: 'https://archive.org/services/img/NASA_NTRS_Archive_20070019742',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'NASA_NTRS_Archive_20070019742',
+    title: 'NASA Technical Reports Server (NTRS) 20070019742: Solar Schematic',
+    image: 'https://archive.org/services/img/NASA_NTRS_Archive_20070019742',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'NASA_NTRS_Archive_20070019742',
+    title: 'NASA Technical Reports Server (NTRS) 20070019742: Solar Schematic',
+    image: 'https://archive.org/services/img/NASA_NTRS_Archive_20070019742',
+  },
+  {
+    id: 'usda-posters',
+    title: 'U.S. Department of Agriculture Poster Collection',
+    image: 'https://archive.org/services/img/CAT31123678',
+  },
+  {
+    id: 'NASA_NTRS_Archive_20070019742',
+    title: 'NASA Technical Reports Server (NTRS) 20070019742: Solar Schematic',
+    image: 'https://archive.org/services/img/NASA_NTRS_Archive_20070019742',
+  },
+];
