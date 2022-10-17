0bbcc4b0ba6df38e8fd566cdefb023a0a4c997b7
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-10-17 11:18:50 -0700

    swap did you know data

--- src/data/did-you-know.ts
+++ src/data/did-you-know.ts
@@ -5,10 +5,10 @@ export type Factoid = {
 export const didYouKnow: Factoid[] = [
   {
     details:
-      "This is just the beginning. Democracy's Library will continue to grow and be updated over time. We'll be adding more information from all over the world, along with useful tools to help find and access what you need.",
+      "Over 50 government organizations have had more than 2000 collections archived by Internet Archive's Archive-it service since 2006.",
   },
   {
     details:
-      "Over 50 government organizations have had more than 2000 collections archived by Internet Archive's Archive-it service since 2006.",
+      "This is just the beginning. Democracy's Library will continue to grow and be updated over time. We'll be adding more information from all over the world, along with useful tools to help find and access what you need.",
   },
 ];
