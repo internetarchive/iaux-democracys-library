7eedca21a2e2edb23c98b14a605fb9bcc86d9700
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-10-17 14:20:06 -0700

    Update text blobs (#9)
    * Factoids: update content shape
    * map card link

--- src/data/did-you-know.ts
+++ src/data/did-you-know.ts
@@ -1,14 +1,25 @@
 export type Factoid = {
   details: string;
+  cardTitle: string;
+  link: string;
+  linkText: string;
+  donateCard?: boolean | undefined;
 };
 
 export const didYouKnow: Factoid[] = [
   {
+    cardTitle: 'About this page',
     details:
       "Over 50 government organizations have had more than 2000 collections archived by Internet Archive's Archive-it service since 2006.",
+    link: 'http://blog.archive.org/2022/09/06/building-democracys-library-celebrate-with-the-internet-archive-on-october-19/',
+    linkText: "Learn more about Democracy's Library",
   },
   {
+    cardTitle: 'About this page',
     details:
       "This is just the beginning. Democracy's Library will continue to grow and be updated over time. We'll be adding more information from all over the world, along with useful tools to help find and access what you need.",
+    link: 'http://blog.archive.org/2022/09/06/building-democracys-library-celebrate-with-the-internet-archive-on-october-19/',
+    linkText: "Learn more about Democracy's Library",
+    donateCard: true,
   },
 ];
---
+++ src/data/resource-card-link.ts
@@ -0,0 +1,4 @@
+export const resourceCardLink: Record<string, string> = {
+  link: 'https://webservices.archive.org/wgw/?',
+  linkText: 'Government websites of the world',
+};
